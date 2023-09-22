fa999421ec7b83ec5018b62b7ab9b133c45314f7
Arun Kumar <cdarun.chinna@gmail.com>
2023-09-22 00:52:44 -0400

    customHook for nowPlayingMovies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,11 @@
 import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+
 import Header from "./Header";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  // Custom Hook
+  useNowPlayingMovies();
 
   return (
     <div>
