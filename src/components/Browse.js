0c1bc93ae0a42bb96d7e88b675ce4e52c8e36c19
Arun Kumar <cdarun.chinna@gmail.com>
2023-09-30 18:46:30 -0400

    Secondary Container with Movies List, Created custom hooks for usePopularMovies, useUpcomingMovies, useTopRatedMovies etc

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatingMovies from "../hooks/useTopRatingMovies";
+import useUpComingMovies from "../hooks/useUpcomingMovies";
 
 import Header from "./Header";
 import MainContainer from "./MainContainer";
@@ -8,6 +11,9 @@ import SecondaryContainer from "./SecondaryContainer";
 const Browse = () => {
   // Custom Hook
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatingMovies();
+  useUpComingMovies();
 
   return (
     <div>
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -10,11 +10,14 @@ const SecondaryContainer = () => {
       <div className="bg-black">
         <div className="-mt-52 pl-12 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
 
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList
+            title={"Top Rated Movies"}
+            movies={movies.topRatedMovies}
+          />
 
-          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Upcoming Movies"} movies={movies.upComingMovies} />
 
           {/**
            * Movies List - Popular
